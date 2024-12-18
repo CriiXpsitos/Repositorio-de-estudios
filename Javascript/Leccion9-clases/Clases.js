bdc9487819dcb0316c722f3c23bd13c5b695ca80
Crixpsitos <cristianpe2609@gmail.com>
2024-12-18 16:54:01 -0500

    Clases-Javascript

---
+++ Javascript/Leccion9-clases/Clases.js
@@ -0,0 +1,62 @@
+class Persona{
+    constructor(nombre, apellido){
+        this._nombre = nombre
+        this._apellido = apellido
+    }
+
+    get nombre() {
+        return this._nombre
+    }
+
+    set nombre(nombre){
+        this._nombre = nombre
+    }
+
+    get apellido(){
+        return this._apellido
+    }
+
+    set apellido(apellido){
+        this._apellido = apellido
+    }
+
+    nombreCompleto(){
+        return `${this._nombre} ${this._apellido}`
+    }
+    toString(){
+        return this.nombreCompleto()
+    }
+}
+
+class Empleado extends Persona{
+    constructor(nombre, apellido, departamento){
+        super(nombre, apellido)
+        this._departamento = departamento
+    }
+
+    get departamento(){
+        return this._departamento
+    }
+
+    set departamento(departamento){
+        this._departamento = departamento
+    }
+    //sobre escritura
+    nombreCompleto(){
+        return `${super.nombreCompleto()}, ${this._departamento}`
+    }
+
+    
+}
+
+
+let persona1 = new Persona("cristian", "peña")
+
+console.log(persona1.nombre)
+persona1.nombre = "Cristian andres"
+console.log(persona1.nombre)
+
+let empleado1 = new Empleado("cristian", "peña", "Desarrollo")
+console.log(empleado1.nombreCompleto())
+
+console.log(empleado1.toString()) 
