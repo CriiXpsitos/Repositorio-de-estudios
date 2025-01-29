81d7a6fd314ed92bb5d78e21ebf9e560ca17be18
Crixpsitos <162711947+Crixpsitos@users.noreply.github.com>
2025-01-29 12:59:27 -0500

    añadi un nuevo componente

--- React/01-gif-expert-app/src/components/GifGrid.tsx
+++ React/01-gif-expert-app/src/components/GifGrid.tsx
@@ -1,15 +1,37 @@
-import { FC } from "react"
+import { FC, useEffect, useState } from "react"
+import type { Gif } from "../helpers/getGif"
 
 interface childProps {
   category: string
 }
 
 
-export const GifGrid: FC<childProps> = ({category}) => {
+export const GifGrid: FC<childProps> = ({ category }) => {
+  const [gifs, setGifs] = useState<Gif[]>([])
+
+  useEffect(() => {
+    const fetchGifs = async (category: string) => {
+      const { getGifs } = await import("../helpers/getGif") // Extrae la función
+      console.log(getGifs) // Asegúrate de que está definida
+      const newGifs = await getGifs(category) // Ahora sí es una función
+      setGifs(newGifs)
+    }
+
+    fetchGifs(category)
+  }, [category])
+
+
   return (
     <>
-    <h3>{category}</h3>
-    <p>Hola mundo</p>
+      <h3>{category}</h3>
+      <div className="card-grid">
+        {gifs.map(({ id, title, url }) => (
+          <div key={id} className="card">
+            <img className="" src={url} alt={title} />
+            <p>{title}</p>
+          </div>
+        ))}
+      </div>
     </>
   )
 }
